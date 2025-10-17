f432d511d34818a0e7390d5099ca705b35f983b4
zjsmyss <yssgefswc@yeah.net>
2025-10-17 15:22:14 +0800

    修改替换 API 地址

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://192.168.55.115:21114".to_owned()
 }
 
 #[inline]
